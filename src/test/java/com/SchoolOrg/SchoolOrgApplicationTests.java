604cfc18b8e68f94b7bf21dd67f42d7622a9286c
davide.spilotros <davide.spilotros@.it.experis.com>
2024-10-28 11:34:17 +0100

    Initial commit

---
+++ src/test/java/com/SchoolOrg/SchoolOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SchoolOrg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolOrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
